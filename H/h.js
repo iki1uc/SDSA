bc203a377820641bdc974ad6aebbffacd4789a18
iki1uc <entertrainer@web.de>
2026-07-30 17:52:33 +0200

    Update and rename h.room to h.js

---
+++ H/h.js
@@ -0,0 +1,17 @@
+import { B } from "./B.js";
+import { H } from "./H.js";
+import { T } from "./T.js";
+
+export const SDSA = {
+    load() {
+        return {
+            axes: 81,
+            vector: "360°",
+            fulfillment: "100%",
+            B,
+            H,
+            T
+        };
+    }
+};
+
